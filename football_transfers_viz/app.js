d812f021db4a8c9cf15bfa3ca8aedb044542cef9
Mads <au635328@uni.au.dk>
2023-10-24 12:00:10 +0200

    wow it does not work

--- football_transfers_viz/app.js
+++ football_transfers_viz/app.js
@@ -3,19 +3,106 @@ const vegaLite require('vega-lite');
 const vegaEmbed require('vega-embed');
 const d3 require('d3');
 
-d3.csv('data\premier-league.csv').then((csvData) => {
+/*d3.csv('data\premier-league.csv').then((csvData) => {
   const data = csvData;
-});
+});*/
 
 const spec = {
-  data: { values: data }, // or csvData
-  mark: 'point',
-  encoding: {
-    x: { field: 'year', type: 'quantitative' },
-    y: { field: 'fee', type: 'quantitative' },
-    color: { field: 'color', type: 'nominal' },
-    size: { field: 'size', type: 'quantitative' },
-  },
+  {
+    "$schema": "https://vega.github.io/schema/vega/v5.json",
+    "description": "A basic scatter plot example depicting transfer statistics.",
+    "width": 200,
+    "height": 200,
+    "padding": 5,
+  
+    "data": [
+      {
+        "name": "source",
+        "url": "data\premier-league.csv",
+      }
+    ],
+  
+    "scales": [
+      {
+        "name": "x",
+        "type": "linear",
+        "round": true,
+        "nice": true,
+        "zero": true,
+        "domain": {"data": "source", "field": "year"},
+        "range": "width"
+      },
+      {
+        "name": "y",
+        "type": "linear",
+        "round": true,
+        "nice": true,
+        "zero": true,
+        "domain": {"data": "source", "field": "fee"},
+        "range": "height"
+      },
+      /*{
+        "name": "size",
+        "type": "linear",
+        "round": true,
+        "nice": false,
+        "zero": true,
+        "domain": {"data": "source", "field": "Acceleration"},
+        "range": [4,361]
+      }*/
+    ],
+  
+    "axes": [
+      {
+        "scale": "x",
+        "grid": true,
+        "domain": false,
+        "orient": "bottom",
+        "tickCount": 5,
+        "title": "Year"
+      },
+      {
+        "scale": "y",
+        "grid": true,
+        "domain": false,
+        "orient": "left",
+        "titlePadding": 5,
+        "title": "fee"
+      }
+    ],
+  
+    /*"legends": [
+      {
+        "size": "size",
+        "title": "Acceleration",
+        "format": "s",
+        "symbolStrokeColor": "#4682b4",
+        "symbolStrokeWidth": 2,
+        "symbolOpacity": 0.5,
+        "symbolType": "circle"
+      }
+    ],*/
+  
+    "marks": [
+      {
+        "name": "marks",
+        "type": "symbol",
+        "from": {"data": "source"},
+        "encode": {
+          "update": {
+            "x": {"scale": "x", "field": "year"},
+            "y": {"scale": "y", "field": "fee"},
+            "size": {"scale": "size", "field": "year"},
+            "shape": {"value": "circle"},
+            "strokeWidth": {"value": 2},
+            "opacity": {"value": 0.5},
+            "stroke": {"value": "#4682b4"},
+            "fill": {"value": "transparent"}
+          }
+        }
+      }
+    ]
+  }
 
 };
 
