ee16f89c72ed83dab82364b435d6376cac20fe7b
nieroo selva <au637543@uni.au.dk>
2023-11-28 18:39:46 +0100

    interactive bubbles

--- football_transfers_viz/scripts/createChart.js
+++ football_transfers_viz/scripts/createChart.js
@@ -21,13 +21,13 @@ fetch(chartSpec)
     vegaEmbed(divId, spec, {"actions": true}
     ).then(result => {
         result.view.addEventListener('click', function(event, item) {
-          createBubbleChart("#col2-2", "test3.json", undefined, item.datum.club_name);
+          createBubbleChart("#col2-2", "test3.json", undefined, item.datum.club_name, item.datum.league_name);
         });
     }).catch(console.warn);
   });
 }
 
-function createBubbleChart(divId, chartSpec, year, clubName) {
+function createBubbleChart(divId, chartSpec, year, clubName, leagueName) {
   var splittedYears = year?.split(",");
   if(splittedYears !== undefined) {
     var yearMin = parseInt(splittedYears[0]);
@@ -41,9 +41,35 @@ fetch(chartSpec)
       spec.signals[0].value = yearMin;
       spec.signals[1].value = yearMax;
     }
+    switch(leagueName) {
+      case "1 Bundesliga":
+        spec.data[0].url = "/data/1-bundesliga.csv"
+        break;
+
+      case "Premier League":
+        spec.data[0].url = "/data/premier-league.csv"
+        break;
+
+      case "Liga Nos":
+        spec.data[0].url = "/data/liga-nos.csv"
+        break;
+      
+      case "Ligue 1":
+        spec.data[0].url = "/data/ligue-1.csv"
+        break;
+
+      case "Primera Division":
+        spec.data[0].url = "/data/primera-division.csv"
+        break;
+
+      case "Serie A":
+        spec.data[0].url = "/data/serie-a.csv"
+        break;
+
+      default:
+        spec.data[0].url = "/data/serie-a.csv";
+    }
     spec.signals[2].value = clubName;
-    console.log(spec.data[0].url);
-    //vegaEmbed(divId, spec, {"actions": true});
     vegaEmbed(divId, spec, {"actions": true});
   });
 }
