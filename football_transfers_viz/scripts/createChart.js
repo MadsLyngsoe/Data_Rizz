25849f3e4d16dbb50175466453c58cf548fdd062
nieroo selva <au637543@uni.au.dk>
2023-11-28 19:42:27 +0100

    made createtreemap and createbubblechart functions

--- football_transfers_viz/scripts/createChart.js
+++ football_transfers_viz/scripts/createChart.js
@@ -18,6 +18,25 @@ fetch(chartSpec)
       spec.signals[1].value = yearMax;
     }
     //vegaEmbed(divId, spec, {"actions": true});
+    vegaEmbed(divId, spec, {"actions": true});
+  });
+  createTreemap("#col2-1", "treemap.json");
+}
+
+function createTreemap(divId, chartSpec, year) {
+  var splittedYears = year?.split(",");
+  if(splittedYears !== undefined) {
+    var yearMin = parseInt(splittedYears[0]);
+    var yearMax = parseInt(splittedYears[1]);
+  }
+fetch(chartSpec)
+  .then((res) => res.json())
+  .then((data) => {
+    spec = data;
+    if(yearMax !== undefined && yearMin !== undefined) {
+      spec.signals[0].value = yearMin;
+      spec.signals[1].value = yearMax;
+    }
     vegaEmbed(divId, spec, {"actions": true}
     ).then(result => {
         result.view.addEventListener('click', function(event, item) {
@@ -26,6 +45,8 @@ fetch(chartSpec)
     }).catch(console.warn);
   });
 }
+
+
 
 function createBubbleChart(divId, chartSpec, year, clubName, leagueName) {
   var splittedYears = year?.split(",");
